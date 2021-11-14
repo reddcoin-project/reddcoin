05583bda68a0425e336e069b0bc85379dc458db1
barrystyle <barrystyle@westnet.com.au>
2021-11-15 02:32:20 +0800

    correctly journal stake data for block undo and recovery on startup

--- src/undo.h
+++ src/undo.h
@@ -24,7 +24,8 @@ struct TxInUndoFormatter
 {
     template<typename Stream>
     void Ser(Stream &s, const Coin& txout) {
-        ::Serialize(s, VARINT(txout.nHeight * uint32_t{2} + txout.fCoinBase ));
+        ::Serialize(s, VARINT(txout.nHeight * uint32_t{4} + txout.fCoinBase + (txout.fCoinStake ? 2u : 0u)));
+        ::Serialize(s, VARINT(txout.nTime));
         if (txout.nHeight > 0) {
             // Required to maintain compatibility with older undo format.
             ::Serialize(s, (unsigned char)0);
@@ -36,8 +37,10 @@ struct TxInUndoFormatter
     void Unser(Stream &s, Coin& txout) {
         uint32_t nCode = 0;
         ::Unserialize(s, VARINT(nCode));
-        txout.nHeight = nCode >> 1;
+        txout.nHeight = nCode >> 2;
         txout.fCoinBase = nCode & 1;
+        txout.fCoinStake = nCode & 2;
+        ::Unserialize(s, VARINT(txout.nTime));
         if (txout.nHeight > 0) {
             // Old versions stored the version number for the last spend of
             // a transaction's outputs. Non-final spends were indicated with
