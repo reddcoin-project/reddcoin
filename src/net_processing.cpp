bfe9c844594fb0793b65125b4f404d2e111e5fd1
Suhas Daftuar <sdaftuar@gmail.com>
2022-02-08 17:36:33 -0500

    Move headers direct fetch to end of ProcessHeadersMessage

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -2228,9 +2228,6 @@ void PeerManagerImpl::ProcessHeadersMessage(CNode& pfrom, const Peer& peer,
             nodestate->m_last_block_announcement = GetTime();
         }
 
-        // Consider immediately downloading blocks.
-        HeadersDirectFetchBlocks(pfrom, pindexLast);
-
         // If we're in IBD, we want outbound peers that will serve us a useful
         // chain. Disconnect peers that are on chains with insufficient work.
         if (m_chainman.ActiveChainstate().IsInitialBlockDownload() && nCount != MAX_HEADERS_RESULTS) {
@@ -2265,6 +2262,9 @@ void PeerManagerImpl::ProcessHeadersMessage(CNode& pfrom, const Peer& peer,
             }
         }
     }
+
+    // Consider immediately downloading blocks.
+    HeadersDirectFetchBlocks(pfrom, pindexLast);
 
     return;
 }
