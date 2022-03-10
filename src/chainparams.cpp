4160d1d249a0bb8ce3f4ab6bf3526e6e45cdffe4
John Nash <redd@redd.ink>
2022-03-10 11:59:17 +1100

    update testnet bech32 address prefix

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -341,7 +341,7 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
 
-        bech32_hrp = "rdd";
+        bech32_hrp = "trdd";
 
         vFixedSeeds.clear();
 
@@ -548,7 +548,7 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};
 
-        bech32_hrp = "rdd";
+        bech32_hrp = "rcrt";
 
         fDefaultConsistencyChecks = true;
         fRequireStandard = true;
@@ -578,13 +578,7 @@ public:
             0
         };
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
-        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,239);
-        base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
-        base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
 
-        bech32_hrp = "bcrt";
     }
 
     /**
