0296bcd0b496c8b8104d3417ddf242a2d09ea2a6
John Nash <redd@redd.ink>
2022-03-17 17:12:29 +1100

    Provide an isolated environment for alpha testing
    a temporary measure to help users in preliminary testing
    
    * throw assert if not running in testnet mode

--- src/init.cpp
+++ src/init.cpp
@@ -792,6 +792,10 @@ bool AppInitParameterInteraction(const ArgsManager& args)
     // Error if network-specific options (-addnode, -connect, etc) are
     // specified in default section of config file, but not overridden
     // on the command line or in this network's section of the config file.
+
+    // temporary measure for others to test wallet in full testnet mode
+    assert (Params().NetworkIDString() == CBaseChainParams::TESTNET);
+
     std::string network = args.GetChainName();
     if (network == CBaseChainParams::SIGNET) {
         LogPrintf("Signet derived magic (message start): %s\n", HexStr(chainparams.MessageStart()));
