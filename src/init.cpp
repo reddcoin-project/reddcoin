f9884be620d17dabfbc85fa8aba714eb50d4e661
John Nash <redd@redd.ink>
2022-03-28 14:53:08 +1100

    rebrand: continued

--- src/init.cpp
+++ src/init.cpp
@@ -108,7 +108,7 @@ static const char* DEFAULT_ASMAP_FILENAME="ip_asn.map";
 /**
  * The PID file facilities.
  */
-static const char* BITCOIN_PID_FILENAME = "bitcoind.pid";
+static const char* BITCOIN_PID_FILENAME = "reddcoind.pid";
 
 static fs::path GetPidFile(const ArgsManager& args)
 {
