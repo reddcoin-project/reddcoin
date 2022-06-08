b214290366a025b1d24164d400bad4be0f49954e
John Nash <redd@redd.ink>
2022-06-08 14:33:00 +1000

    set testnet to 30 blocks for COINBASE_MATURITY

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -283,7 +283,7 @@ public:
         consensus.signet_challenge.clear();
         consensus.nSubsidyHalvingInterval = 210000;
         consensus.nLastPowHeight = 1439;
-        consensus.nCoinbaseMaturity = 50;
+        consensus.nCoinbaseMaturity = 30;
         consensus.BIP16Exception = uint256S("0x00000000000002dc756eebf4f49723ed8d30cc28a5f108eb94b1ba88ac4f9c22");
         consensus.BIP34Height = 227931;
         consensus.BIP34Hash = uint256S("0x000000000000024b89b42a942fe0d9fea3bb44ab7bd1b19115dd6a759c0808b8");
