f520f8b78e4f63963e4556389b4b573247479f42
John Nash <john@redd.ink>
2022-10-14 18:17:20 +1100

    adjust fees to better align with reddcoin

--- src/validation.h
+++ src/validation.h
@@ -62,7 +62,7 @@ struct AssumeutxoData;
 static const int64_t COIN_YEAR_REWARD = 5 * CENT; // 5% per year
 
 /** Default for -minrelaytxfee, minimum relay fee for transactions */
-static const unsigned int DEFAULT_MIN_RELAY_TX_FEE = 1000;
+static const unsigned int DEFAULT_MIN_RELAY_TX_FEE = 100000;
 /** Default for -limitancestorcount, max number of in-mempool ancestors */
 static const unsigned int DEFAULT_ANCESTOR_LIMIT = 25;
 /** Default for -limitancestorsize, maximum kilobytes of tx + all in-mempool ancestors */
