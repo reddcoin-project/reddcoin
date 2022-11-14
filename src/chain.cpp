62e296c8e6301dfcd4e59b2cf0bdebff1f26e5a4
John Nash <john@redd.ink>
2022-11-14 11:59:28 +1100

    [consensus] remove ISM
    Now that IsSuperMajority no longer required, remove

--- src/chain.cpp
+++ src/chain.cpp
@@ -119,28 +119,6 @@ void CBlockIndex::BuildSkip()
         pskip = pprev->GetAncestor(GetSkipHeight(nHeight));
 }
 
-bool CBlockIndex::IsSuperMajority(int minVersion, const CBlockIndex* pstart, unsigned int nRequired)
-{
-    unsigned int nToCheck = 0;
-
-    if (minVersion == 3) {
-        nToCheck = 10000;
-    } else if (minVersion == 4) {
-        nToCheck = 7200;
-    } else if (minVersion == 5) {
-        nToCheck = 10000;
-    }
-
-    unsigned int nFound = 0;
-    for (unsigned int i = 0; i < nToCheck && nFound < nRequired && pstart != nullptr; i++) {
-        if (pstart->nVersion >= minVersion)
-            ++nFound;
-        pstart = pstart->pprev;
-    }
-
-    return (nFound >= nRequired);
-}
-
 arith_uint256 GetBlockTrust(const CBlockIndex& block)
 {
     arith_uint256 bnTarget;
