cf53594d62aa767786426f77add1ca8bbcfee8cb
John Nash <john@redd.ink>
2026-01-15 17:40:29 +1100

    test: Adapt p2p_getaddr_caching for ReddCoin PoS and mocktime
    - Add wallet requirement check
    - Use ReddCoin regtest port (45444) instead of Bitcoin (8333)
    - Initialize mocktime from cache block time
    - Add -whitelist=127.0.0.1 to prevent timeout disconnections

--- test/functional/p2p_getaddr_caching.py
+++ test/functional/p2p_getaddr_caching.py
@@ -4,8 +4,6 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test addr response caching"""
 
-import time
-
 from test_framework.messages import msg_getaddr
 from test_framework.p2p import (
     P2PInterface,
@@ -13,8 +11,12 @@ from test_framework.p2p import (
 )
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import (
+    advance_time_for_pos,
     assert_equal,
 )
+
+# ReddCoin regtest port (Bitcoin uses 8333)
+REDDCOIN_REGTEST_PORT = 45444
 
 # As defined in net_processing.
 MAX_ADDR_TO_SEND = 1000
@@ -42,14 +44,23 @@ class AddrReceiver(P2PInterface):
 class AddrTest(BitcoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 1
+        # Whitelist test connections to prevent mocktime timeout disconnections
+        self.extra_args = [['-whitelist=127.0.0.1']]
+
+    def skip_test_if_missing_module(self):
+        self.skip_if_no_wallet()
 
     def run_test(self):
+        # Initialize mocktime from cache block time for ReddCoin PoS compatibility
+        self.mocktime = self.nodes[0].getblockheader(self.nodes[0].getbestblockhash())['time'] + 600
+        self.nodes[0].setmocktime(self.mocktime)
+
         self.log.info('Fill peer AddrMan with a lot of records')
         for i in range(10000):
             first_octet = i >> 8
             second_octet = i % 256
             a = "{}.{}.1.1".format(first_octet, second_octet)
-            self.nodes[0].addpeeraddress(a, 8333)
+            self.nodes[0].addpeeraddress(a, REDDCOIN_REGTEST_PORT)
 
         # Need to make sure we hit MAX_ADDR_TO_SEND records in the addr response later because
         # only a fraction of all known addresses can be cached and returned.
@@ -58,28 +69,27 @@ class AddrTest(BitcoinTestFramework):
         responses = []
         self.log.info('Send many addr requests within short time to receive same response')
         N = 5
-        cur_mock_time = int(time.time())
         for i in range(N):
             addr_receiver = self.nodes[0].add_p2p_connection(AddrReceiver())
             addr_receiver.send_and_ping(msg_getaddr())
             # Trigger response
-            cur_mock_time += 5 * 60
-            self.nodes[0].setmocktime(cur_mock_time)
+            self.mocktime += 5 * 60
+            self.nodes[0].setmocktime(self.mocktime)
             addr_receiver.wait_until(addr_receiver.addr_received)
             responses.append(addr_receiver.get_received_addrs())
         for response in responses[1:]:
             assert_equal(response, responses[0])
         assert(len(response) == MAX_ADDR_TO_SEND)
 
-        cur_mock_time += 3 * 24 * 60 * 60
-        self.nodes[0].setmocktime(cur_mock_time)
+        self.mocktime += 3 * 24 * 60 * 60
+        self.nodes[0].setmocktime(self.mocktime)
 
         self.log.info('After time passed, see a new response to addr request')
         last_addr_receiver = self.nodes[0].add_p2p_connection(AddrReceiver())
         last_addr_receiver.send_and_ping(msg_getaddr())
         # Trigger response
-        cur_mock_time += 5 * 60
-        self.nodes[0].setmocktime(cur_mock_time)
+        self.mocktime += 5 * 60
+        self.nodes[0].setmocktime(self.mocktime)
         last_addr_receiver.wait_until(last_addr_receiver.addr_received)
         # new response is different
         assert(set(responses[0]) != set(last_addr_receiver.get_received_addrs()))
