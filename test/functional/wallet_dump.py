dd94a603f9f5bbf1dea45116ffdd6023260210bf
John Nash <john@redd.ink>
2026-03-16 22:13:04 +1100

    test: Adapt wallet_dump for ReddCoin regtest address prefixes and PoS
    - Remove custom setup_network so base class imports cache staking keys
    - Fix address prefixes in read_dump(): rcrt1 (bech32), 3 (P2SH), r (P2PKH)
    - Use explicit wallet RPC handles (dump_wallet, w2) for wallet operations
    - Derive mocktime from block header after generate() instead of time.time()
    - Restore mocktime after restart_node and advance for periodic flush trigger
    - Remove unused import time

--- test/functional/wallet_dump.py
+++ test/functional/wallet_dump.py
@@ -5,7 +5,6 @@
 """Test the dumpwallet RPC."""
 import datetime
 import os
-import time
 
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import (
@@ -67,14 +66,14 @@ def read_dump(file_name, addrs, script_addrs, hd_master_addr_old):
                 # count key types
                 for addrObj in addrs:
                     if addrObj['address'] == addr.split(",")[0] and addrObj['hdkeypath'] == keypath and keytype == "label=":
-                        if addr.startswith('m') or addr.startswith('n'):
-                            # P2PKH address
-                            found_legacy_addr += 1
-                        elif addr.startswith('2'):
+                        if addr.startswith('rcrt1'):
+                            found_bech32_addr += 1
+                        elif addr.startswith('3'):
                             # P2SH-segwit address
                             found_p2sh_segwit_addr += 1
-                        elif addr.startswith('bcrt1'):
-                            found_bech32_addr += 1
+                        elif addr.startswith('r'):
+                            # P2PKH address
+                            found_legacy_addr += 1
                         break
                     elif keytype == "change=1":
                         found_addr_chg += 1
@@ -101,12 +100,9 @@ class WalletDumpTest(BitcoinTestFramework):
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
 
-    def setup_network(self):
-        self.add_nodes(self.num_nodes, extra_args=self.extra_args)
-        self.start_nodes()
-
     def run_test(self):
         self.nodes[0].createwallet("dump")
+        dump_wallet = self.nodes[0].get_wallet_rpc("dump")
 
         wallet_unenc_dump = os.path.join(self.nodes[0].datadir, "wallet.unencrypted.dump")
         wallet_enc_dump = os.path.join(self.nodes[0].datadir, "wallet.encrypted.dump")
@@ -119,23 +115,24 @@ class WalletDumpTest(BitcoinTestFramework):
         addrs = []
         for address_type in ['legacy', 'p2sh-segwit', 'bech32']:
             for _ in range(test_addr_count):
-                addr = self.nodes[0].getnewaddress(address_type=address_type)
-                vaddr = self.nodes[0].getaddressinfo(addr)  # required to get hd keypath
+                addr = dump_wallet.getnewaddress(address_type=address_type)
+                vaddr = dump_wallet.getaddressinfo(addr)  # required to get hd keypath
                 addrs.append(vaddr)
 
         # Test scripts dump by adding a 1-of-1 multisig address
-        multisig_addr = self.nodes[0].addmultisigaddress(1, [addrs[1]["address"]])["address"]
+        multisig_addr = dump_wallet.addmultisigaddress(1, [addrs[1]["address"]])["address"]
 
         # Refill the keypool. getnewaddress() refills the keypool *before* taking a key from
         # the keypool, so the final call to getnewaddress leaves the keypool with one key below
         # its capacity
-        self.nodes[0].keypoolrefill()
+        dump_wallet.keypoolrefill()
 
         self.log.info('Mine a block one second before the wallet is dumped')
-        dump_time = int(time.time())
-        self.nodes[0].setmocktime(dump_time - 1)
         self.nodes[0].generate(1)
+        block_time = self.nodes[0].getblockheader(self.nodes[0].getbestblockhash())['time']
+        dump_time = block_time + 1
         self.nodes[0].setmocktime(dump_time)
+        self.nodes[0].mocktime = dump_time
         dump_time_str = '# * Created on {}Z'.format(
             datetime.datetime.fromtimestamp(
                 dump_time,
@@ -147,12 +144,12 @@ class WalletDumpTest(BitcoinTestFramework):
         )
         dump_best_block_2 = '#   mined on {}Z'.format(
             datetime.datetime.fromtimestamp(
-                dump_time - 1,
+                block_time,
                 tz=datetime.timezone.utc,
             ).replace(tzinfo=None).isoformat())
 
         self.log.info('Dump unencrypted wallet')
-        result = self.nodes[0].dumpwallet(wallet_unenc_dump)
+        result = dump_wallet.dumpwallet(wallet_unenc_dump)
         assert_equal(result['filename'], wallet_unenc_dump)
 
         found_comments, found_legacy_addr, found_p2sh_segwit_addr, found_bech32_addr, found_script_addr, found_addr_chg, found_addr_rsv, hd_master_addr_unenc = \
@@ -168,12 +165,12 @@ class WalletDumpTest(BitcoinTestFramework):
         assert_equal(found_addr_chg, 0)  # 0 blocks where mined
         assert_equal(found_addr_rsv, 90 * 2)  # 90 keys plus 100% internal keys
 
-        # encrypt wallet, restart, unlock and dump
-        self.nodes[0].encryptwallet('test')
-        self.nodes[0].walletpassphrase('test', 100)
+        # encrypt wallet, unlock and dump
+        dump_wallet.encryptwallet('test')
+        dump_wallet.walletpassphrase('test', 100)
         # Should be a no-op:
-        self.nodes[0].keypoolrefill()
-        self.nodes[0].dumpwallet(wallet_enc_dump)
+        dump_wallet.keypoolrefill()
+        dump_wallet.dumpwallet(wallet_enc_dump)
 
         found_comments, found_legacy_addr, found_p2sh_segwit_addr, found_bech32_addr, found_script_addr, found_addr_chg, found_addr_rsv, _ = \
             read_dump(wallet_enc_dump, addrs, [multisig_addr], hd_master_addr_unenc)
@@ -189,25 +186,31 @@ class WalletDumpTest(BitcoinTestFramework):
         assert_equal(found_addr_rsv, 90 * 2)
 
         # Overwriting should fail
-        assert_raises_rpc_error(-8, "already exists", lambda: self.nodes[0].dumpwallet(wallet_enc_dump))
+        assert_raises_rpc_error(-8, "already exists", lambda: dump_wallet.dumpwallet(wallet_enc_dump))
 
         # Restart node with new wallet, and test importwallet
         self.restart_node(0)
+        self.nodes[0].setmocktime(dump_time)
+        self.nodes[0].mocktime = dump_time
         self.nodes[0].createwallet("w2")
+        w2 = self.nodes[0].get_wallet_rpc("w2")
 
         # Make sure the address is not IsMine before import
-        result = self.nodes[0].getaddressinfo(multisig_addr)
+        result = w2.getaddressinfo(multisig_addr)
         assert not result['ismine']
 
-        self.nodes[0].importwallet(wallet_unenc_dump)
+        w2.importwallet(wallet_unenc_dump)
 
         # Now check IsMine is true
-        result = self.nodes[0].getaddressinfo(multisig_addr)
+        result = w2.getaddressinfo(multisig_addr)
         assert result['ismine']
 
         self.log.info('Check that wallet is flushed')
         with self.nodes[0].assert_debug_log(['Flushing wallet.dat'], timeout=20):
-            self.nodes[0].getnewaddress()
+            w2.getnewaddress()
+            # Advance mocktime so periodic flush condition (2s since last update) triggers
+            self.nodes[0].setmocktime(dump_time + 10)
+            self.nodes[0].mocktime = dump_time + 10
 
         # Make sure that dumpwallet doesn't have a lock order issue when there is an unconfirmed tx and it is reloaded
         # See https://github.com/bitcoin/bitcoin/issues/22489
